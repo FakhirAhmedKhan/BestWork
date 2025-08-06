52a88881f5794971c1a922e024adcaf866365d59
Fakhir Ahmed Khan <fakhirahmed898@gmail.com>
2025-08-06 14:53:21 +0400

    .

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/BestWork",
-  plugins: [react(), tailwindcss(), visualizer({ open: true })],
+  plugins: [react(), tailwindcss()],
 });
